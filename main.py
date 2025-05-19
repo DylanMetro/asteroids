4c12fe7a77dc71019fe64d5156d68d42e8cc8c03
DylanMetro <dylan.bice@my.metrostate.edu>
2025-05-19 18:38:22 -0500

    Set framerate

--- main.py
+++ main.py
@@ -4,14 +4,19 @@ from constants import *
 def main():
     pygame.init()
 
+    dps = pygame.time.Clock()
+
+    dt = 0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0,0,0))
+        screen.fill("black")
         pygame.display.flip()
+        dt = dps.tick(60)/1000
 
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
